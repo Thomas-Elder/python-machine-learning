9186498912def783173b545bd699b1f960e12c3c
Thomas <thombsaway@gmail.com>
2020-07-22 15:30:24 +1000

    Started Machine Learning Set up
    Just started the first few videos on setting up and getting all the files in order.
    
    There are a few new things to get my head around before jumping in.
    
    Anyway this commit, I created a testing.py file just to try a couple things out.
    
    I also created progress.md as I thought it'd be easier to organise that
    info if I could put headings etc.

---
+++ testing.py
@@ -0,0 +1,16 @@
+# %%
+print('hello things!')
+
+# %%
+print('hello other things?')
+
+# %%
+var = 'even more things'
+otherVar = 'yet more things'
+
+# %%
+newVar = 'Let us do ' + var + ' and ' + otherVar + ' and see what the shell makes of it'
+
+# %%
+# and can just write code like normal... can I run the whole script like normal?
+# yes you can
