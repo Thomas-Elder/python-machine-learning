68687bc1dffc3a66af23820068aba56508d00522
Thomas <thombsaway@gmail.com>
2020-07-22 20:41:33 +1000

    Added encoded the categorical data as numerical data

--- doing/part 1/pre.py
+++ doing/part 1/pre.py
@@ -4,8 +4,14 @@
 import numpy
 import matplotlib.pyplot as pyplot
 import pandas
-from sklearn.impute import SimpleImputer
 
+# SciKit imports
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+# Misc imports
 import os
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -36,4 +42,22 @@ features[:, 1:3] = imputer.transform(features[:, 1:3])
 #%% Check sets
 # The previously blank entries are now the mean value for their columns
 logging.debug('Value of features: %s' % (features))
+logging.debug('Value of dependantVariable: %s' % (dependantVariable))
+
+#%% Encoding the categorical data, first the independant variables.
+columnTransformer = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+features = numpy.array(columnTransformer.fit_transform(features))
+
+#%% Check sets
+# The country data has been replaced by 3 columns, representing the 3 
+# countries in that column. So column 1 is France, and has a 1 value in 
+# the rows that contained the country France.
+logging.debug('Value of features: %s' % (features))
+
+#%% Encoding the categorical data, then the independent variables
+labelEncoder = LabelEncoder()
+dependantVariable = labelEncoder.fit_transform(dependantVariable)
+
+#%% Check sets
+# The purchase data has been replaced with 1 or 0 for yes/no
 logging.debug('Value of dependantVariable: %s' % (dependantVariable))
