aa668a3ec7998a54c52ddc643999e85165a4fa74
Thomas <thombsaway@gmail.com>
2020-07-29 10:40:03 +1000

    Stubbed svr model, importing the data

---
+++ doing/part 2/section 7 support vector regression/supportVectorRegression.py
@@ -0,0 +1,32 @@
+#! python3
+
+#%% Imports
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+
+# Misc imports
+import os
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.CRITICAL)
+
+# Load data
+logging.debug('cwd: %s' % (os.getcwd()))
+dataset = pandas.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature scaling
+
+
+# Training the svr model
+
+
+# Predict the result
+
+
+# Visualise the result
+
+
+# Visualise the high res result
