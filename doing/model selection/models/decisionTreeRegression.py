23eee74f3f8e5d09e62acc02099c76f4b83da664
Thomas <thombsaway@gmail.com>
2020-08-03 20:14:11 +1000

    Refactor decision tree to function

--- doing/model selection/models/decisionTreeRegression.py
+++ doing/model selection/models/decisionTreeRegression.py
@@ -18,19 +18,22 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.disable(logging.CRITICAL)
 
-# Load data
-logging.debug('cwd: %s' % (os.getcwd()))
-dataset = pandas.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+def modelSelection_DecisionTreeRegression(file: str):
 
-# Split into training and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    # Load data
+    logging.debug('cwd: %s' % (os.getcwd()))
+    dataset = pandas.read_csv('Data.csv')
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
 
-# Regress
-regressor = DecisionTreeRegressor(random_state=0)
-regressor.fit(X_train, y_train)
+    # Split into training and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-y_pred = regressor.predict(X_test)
+    # Regress
+    regressor = DecisionTreeRegressor(random_state=0)
+    regressor.fit(X_train, y_train)
 
-print(r2_score(y_test, y_pred)) # 0.9342783714449767
+    # Predict
+    y_pred = regressor.predict(X_test)
+
+    return r2_score(y_test, y_pred)
