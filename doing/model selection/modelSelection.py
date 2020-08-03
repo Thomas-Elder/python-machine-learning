e8338c7c345b94eb532f08e7b28c7076440516a5
Thomas <thombsaway@gmail.com>
2020-08-03 20:31:56 +1000

    Refactor main, models managed as a list of dicts, best model by R^2 computed

--- doing/model selection/modelSelection.py
+++ doing/model selection/modelSelection.py
@@ -5,15 +5,24 @@ from models import supportVectorRegression
 from models import decisionTreeRegression
 from models import randomForestRegression
 
-linearRegression_R2 = linearRegression.modelSelection_LinearRegression('Data.csv')
-polynomialRegression_R2 = polynomialRegression.modelSelection_PolynomialRegression('Data.csv')
-supportVectorRegression_R2 = supportVectorRegression.modelSelection_SupportVectorRegression('Data.csv')
-decisionTreeRegression_R2 = decisionTreeRegression.modelSelection_DecisionTreeRegression('Data.csv')
-randomForestRegression_R2 = randomForestRegression.modelSelection_RandomForestRegression('Data.csv')
+models = []
+
+models.append({'model': 'linearRegression', 'R^2': linearRegression.modelSelection_LinearRegression('Data.csv')})
+models.append({'model': 'polynomialRegression', 'R^2': polynomialRegression.modelSelection_PolynomialRegression('Data.csv')})
+models.append({'model': 'supportVectorRegression', 'R^2': supportVectorRegression.modelSelection_SupportVectorRegression('Data.csv')})
+models.append({'model': 'decisionTreeRegression', 'R^2': decisionTreeRegression.modelSelection_DecisionTreeRegression('Data.csv')})
+models.append({'model': 'randomForestRegression', 'R^2': randomForestRegression.modelSelection_RandomForestRegression('Data.csv')})
+
+best = {'model': '', 'R^2': 0}
 
 print()
-print('{:<30}:{}'.format('linearRegression_R2', linearRegression_R2))
-print('{:<30}:{}'.format('polynomialRegression_R2', polynomialRegression_R2))
-print('{:<30}:{}'.format('supportVectorRegression_R2', supportVectorRegression_R2))
-print('{:<30}:{}'.format('decisionTreeRegression_R2', decisionTreeRegression_R2))
-print('{:<30}:{}'.format('randomForestRegression_R2', randomForestRegression_R2))
+print('Here are the results for the 5 models tested:')
+
+for model in models:
+    print('{:<30}:{}'.format(model['model'], model['R^2']))
+    
+    if model['R^2'] > best['R^2']:
+        best = model
+
+print()
+print('The best performing model was:{} with an R^2 of {}'.format(best['model'], best['R^2']))
