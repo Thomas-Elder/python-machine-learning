a6efc6006f8965f8bc3fc218a3964b2530c3a458
Thomas <thombsaway@gmail.com>
2020-08-03 20:15:10 +1000

    Refactor support vector to function

--- doing/model selection/models/supportVectorRegression.py
+++ doing/model selection/models/supportVectorRegression.py
@@ -16,46 +16,48 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 #logging.disable(logging.CRITICAL)
 
-# Load data
-logging.debug('cwd: %s' % (os.getcwd()))
-dataset = pandas.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+def modelSelection_SupportVectorRegression(file: str):
 
-# Split into training and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    # Load data
+    logging.debug('cwd: %s' % (os.getcwd()))
+    dataset = pandas.read_csv('Data.csv')
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
 
-logging.info('Len of X_train: \n{}'.format(len(X_train)))
-logging.info('Value of X_train: \n{}'.format(X_train))
-logging.info('Value of X_test: \n{}'.format(X_test))
-logging.info('Len of y_train: \n{}'.format(len(y_train)))
-logging.info('Value of y_train: \n{}'.format(y_train))
-logging.info('Value of y_test: \n{}'.format(y_test))
+    # Split into training and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-# Feature scaling. Will need to transform the y set to a 2d array, as this 
-# is what the scaler accepts as a parameter
-#y_train = y_train.reshape(len(y),1)
+    logging.info('Len of X_train: \n{}'.format(len(X_train)))
+    logging.info('Value of X_train: \n{}'.format(X_train))
+    logging.info('Value of X_test: \n{}'.format(X_test))
+    logging.info('Len of y_train: \n{}'.format(len(y_train)))
+    logging.info('Value of y_train: \n{}'.format(y_train))
+    logging.info('Value of y_test: \n{}'.format(y_test))
 
-# We need separate scalers as they calculate the mean of the column to 
-# scale the values. We want separate means for this. X first.
-standardScaler_X_train = StandardScaler()
-standardScaler_X_train.fit(X)
-X_train_scaled = standardScaler_X_train.transform(X_train)
-X_test_scaled = standardScaler_X_train.transform(X_test)
+    # Feature scaling. Will need to transform the y set to a 2d array, as this 
+    # is what the scaler accepts as a parameter
+    #y_train = y_train.reshape(len(y),1)
 
-# Then y? Not sure here tbh
-#standardScaler_y_train = StandardScaler()
-#standardScaler_y_train.fit(y_train)
-#y_train_scaled = standardScaler_y_train.transform(y_train)
+    # We need separate scalers as they calculate the mean of the column to 
+    # scale the values. We want separate means for this. X first.
+    standardScaler_X_train = StandardScaler()
+    standardScaler_X_train.fit(X)
+    X_train_scaled = standardScaler_X_train.transform(X_train)
+    X_test_scaled = standardScaler_X_train.transform(X_test)
 
-logging.info('Len of X_train_scaled: \n{}'.format(len(X_train_scaled)))
-logging.info('Value of X_train_scaled: \n{}'.format(X_train_scaled))
+    # Then y? Not sure here tbh
+    #standardScaler_y_train = StandardScaler()
+    #standardScaler_y_train.fit(y_train)
+    #y_train_scaled = standardScaler_y_train.transform(y_train)
 
-# Training the svr model
-regressor = SVR(kernel='rbf')
-regressor.fit(X_train_scaled, y_train)
+    logging.info('Len of X_train_scaled: \n{}'.format(len(X_train_scaled)))
+    logging.info('Value of X_train_scaled: \n{}'.format(X_train_scaled))
 
-# Predict the result, then unscale the result 
-y_pred = regressor.predict(X_test_scaled)
+    # Training the svr model
+    regressor = SVR(kernel='rbf')
+    regressor.fit(X_train_scaled, y_train)
 
-print(r2_score(y_test, y_pred)) # 0.9431332255808436
+    # Predict the result, then unscale the result 
+    y_pred = regressor.predict(X_test_scaled)
+
+    return r2_score(y_test, y_pred)
