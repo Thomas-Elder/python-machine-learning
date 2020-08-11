75bd8aa61606b2308531a861f6cf9d6e95efd88b
Thomas <thombsaway@gmail.com>
2020-08-11 13:48:46 +1000

    Add visualisation for training and test sets

--- doing/part 3/section 16 logistic regression/logisticRegression.py
+++ doing/part 3/section 16 logistic regression/logisticRegression.py
@@ -59,3 +59,37 @@ print(cm)
 
 acc = accuracy_score(y_test, y_pred)
 print(acc) # 0.89 - number of correct predictions divided by total number of predictions, so 89% correct.
+
+# Visualising the training set
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = numpy.meshgrid(numpy.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                     numpy.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, model.predict(standardScaler.transform(numpy.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(numpy.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = numpy.meshgrid(numpy.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1), 
+numpy.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, model.predict(standardScaler.transform(numpy.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(numpy.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
