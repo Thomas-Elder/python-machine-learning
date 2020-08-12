d79506b6c6cbfb8d869665050ea2f4de92a697c4
Thomas <thombsaway@gmail.com>
2020-08-12 12:45:23 +1000

    Start K nearest neighbours

---
+++ doing/part 3/section 17 k nearest neighbour/knn.py
@@ -0,0 +1,95 @@
+#! python3
+
+# Ok so we have data on people who did or didn't buy an SUV.
+# The dependent var is whether or not they purchased, 0 or 1. The features are 
+# their age and salary.
+
+#%% Imports
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Misc imports
+import os
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.CRITICAL)
+
+# Load data
+logging.debug('cwd: %s' % (os.getcwd()))
+dataset = pandas.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Scale
+standardScaler = StandardScaler()
+X_train_scaled = standardScaler.fit_transform(X_train)
+X_test_scaled = standardScaler.transform(X_test)
+
+# Model
+model = KNeighborsClassifier()
+model.fit(X_train_scaled, y_train)
+
+# Predict range
+y_pred = model.predict(X_test_scaled)
+
+# Predict single value
+x_single = standardScaler.transform([[30, 87000]])
+y_pred_single = model.predict(x_single)
+
+# Compare results
+numpy.set_printoptions(precision=2)
+print(numpy.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+print('y_pred_single:{}'.format(y_pred_single)) # predicted 0, no buy, correct
+
+# Create confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+# [[64  4] - 64 correct nobuy predictions, 4 incorrect
+# [ 3 29]] - 3 incorrect buy predictions, 29 correct
+
+acc = accuracy_score(y_test, y_pred)
+print(acc) # 0.93 - number of correct predictions divided by total number of predictions, so 93% correct.
+
+# Visualising the training set
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = numpy.meshgrid(numpy.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                     numpy.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, model.predict(standardScaler.transform(numpy.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(numpy.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = numpy.meshgrid(numpy.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1), 
+numpy.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, model.predict(standardScaler.transform(numpy.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(numpy.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
