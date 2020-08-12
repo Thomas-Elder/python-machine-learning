32b60bd49f164a7fee3f34d66e84befc8072eb1a
Thomas <thombsaway@gmail.com>
2020-08-12 13:23:19 +1000

    Implement knn

--- doing/part 3/section 17 k nearest neighbour/knn.py
+++ doing/part 3/section 17 k nearest neighbour/knn.py
@@ -5,9 +5,9 @@
 # their age and salary.
 
 #%% Imports
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-import pandas
+import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
@@ -22,7 +22,7 @@ logging.disable(logging.CRITICAL)
 
 # Load data
 logging.debug('cwd: %s' % (os.getcwd()))
-dataset = pandas.read_csv('Social_Network_Ads.csv')
+dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
@@ -30,24 +30,24 @@ y = dataset.iloc[:, -1].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Scale
-standardScaler = StandardScaler()
-X_train_scaled = standardScaler.fit_transform(X_train)
-X_test_scaled = standardScaler.transform(X_test)
+sc = StandardScaler()
+X_train_scaled = sc.fit_transform(X_train)
+X_test_scaled = sc.transform(X_test)
 
 # Model
-model = KNeighborsClassifier()
-model.fit(X_train_scaled, y_train)
+classifier = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+classifier.fit(X_train_scaled, y_train)
 
 # Predict range
-y_pred = model.predict(X_test_scaled)
+y_pred = classifier.predict(X_test_scaled)
 
 # Predict single value
-x_single = standardScaler.transform([[30, 87000]])
-y_pred_single = model.predict(x_single)
+x_single = sc.transform([[30, 87000]])
+y_pred_single = classifier.predict(x_single)
 
 # Compare results
-numpy.set_printoptions(precision=2)
-print(numpy.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 print('y_pred_single:{}'.format(y_pred_single)) # predicted 0, no buy, correct
 
@@ -61,17 +61,18 @@ acc = accuracy_score(y_test, y_pred)
 print(acc) # 0.93 - number of correct predictions divided by total number of predictions, so 93% correct.
 
 # Visualising the training set
+# Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
-X1, X2 = numpy.meshgrid(numpy.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
-                     numpy.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
-plt.contourf(X1, X2, model.predict(standardScaler.transform(numpy.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(numpy.unique(y_set)):
+for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
+plt.title('K-NN (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -80,15 +81,15 @@ plt.show()
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
-X1, X2 = numpy.meshgrid(numpy.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1), 
-numpy.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
-plt.contourf(X1, X2, model.predict(standardScaler.transform(numpy.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(numpy.unique(y_set)):
+for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
+plt.title('K-NN (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
