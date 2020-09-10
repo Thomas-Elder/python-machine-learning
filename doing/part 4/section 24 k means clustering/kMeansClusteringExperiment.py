4da3d0228331466548c4b858366434a4fee4f7e7
Thomas Elder <thombsaway@gmail.com>
2020-09-10 17:14:08 +1000

    Fix encoding for gender column

--- doing/part 4/section 24 k means clustering/kMeansClusteringExperiment.py
+++ doing/part 4/section 24 k means clustering/kMeansClusteringExperiment.py
@@ -10,31 +10,38 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # Imports to encode the categorical data
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 import os
 import logging
 
 # - run a regression of some sort on each column vs spending, to identify the most indicative feature
-# sex
+# gender
 print()
-print(f'Column by column, firstly, sex')
+print(f'Column by column, firstly, gender')
 dataset = pd.read_csv('Mall_Customers.csv')
-X = dataset.iloc[:, 1:2].values
+X = dataset.iloc[:, 1].values
 y = dataset.iloc[:, -1].values
 
-# Need to convert the sex column into categories... #todo
-columnTransformer = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-X = np.array(columnTransformer.fit_transform(X))
+# So this turns X into a number of columns representing the different possible states, in this case, male or female.
+# It drops the first column, to avoid the dummy variable trap. So here we end up with one column for Male, and if it's
+# 1 it's male, if it's 0 it's female.
+X = pd.get_dummies(data=X, drop_first=True)
 
 # Split into training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
+print(X_train)
+print(y_train)
+
 # Regress
-regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor = LinearRegression()
+#RandomForestRegressor(n_estimators=10, random_state=0)
 regressor.fit(X_train, y_train)
 
 # Predict
@@ -44,6 +51,17 @@ y_pred = regressor.predict(X_test)
 r2 = r2_score(y_test, y_pred)
 
 print(f'r2: {r2}')
+
+print(X)
+print(y)
+
+# Visualise
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Salary vs Job Level - Simple Linear')
+plt.xlabel('Level of Job')
+plt.ylabel('Salary')
+plt.show()
 
 # age
 print()
