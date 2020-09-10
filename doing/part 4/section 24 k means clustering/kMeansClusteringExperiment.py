b563548549612da75ff0311bc5c74646976e48fb
Thomas Elder <thombsaway@gmail.com>
2020-09-10 16:53:34 +1000

    Add regression for income vs spending

--- doing/part 4/section 24 k means clustering/kMeansClusteringExperiment.py
+++ doing/part 4/section 24 k means clustering/kMeansClusteringExperiment.py
@@ -97,10 +97,6 @@ dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, 2:3].values
 y = dataset.iloc[:, -1].values
 
-# Need to convert the sex column into categories... #todo
-#columnTransformer = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-#X = np.array(columnTransformer.fit_transform(X))
-
 # Split into training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
@@ -117,6 +113,25 @@ r2 = r2_score(y_test, y_pred)
 print(f'r2: {r2}')
 
 # income
+print()
+print(f'Thirdly, income')
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, 3:4].values
+y = dataset.iloc[:, -1].values
 
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Regress
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(X_train, y_train)
+
+# Predict
+y_pred = regressor.predict(X_test)
+
+# Test
+r2 = r2_score(y_test, y_pred)
+
+print(f'r2: {r2}')
 
 # - run kmeans on other features to see other clusters?
